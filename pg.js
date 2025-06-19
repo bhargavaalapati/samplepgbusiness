67afa1f762fef64bbe55c99134a743193cc20d6f
bhargavaalapati <165909926+bhargavaalapati@users.noreply.github.com>
2025-06-20 00:36:32 +0530

    Add files via upload

---
+++ pg.js
@@ -0,0 +1,4 @@
+document.getElementById('mobile-menu-btn').addEventListener('click', () => {
+    const mobileMenu = document.getElementById('mobile-menu');
+    mobileMenu.classList.toggle('hidden');
+});
